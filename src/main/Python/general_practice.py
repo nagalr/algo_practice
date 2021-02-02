b4250bf79397d6228edfecb5716b55b71e92b990
Ronnen Nagal <nagalr@gmail.com>
2021-02-02 15:38:36 +0200

    Add a third version to 'two_sum' function

--- src/main/Python/general_practice.py
+++ src/main/Python/general_practice.py
@@ -1,6 +1,7 @@
 import timeit
 
 
+#
 def two_sum(nums, target):
     for i in range(len(nums)):
         for j in range(len(nums)):
@@ -23,7 +24,31 @@ def two_sum2(nums, target):
     return None
 
 
-print("***** time-of-first-method ******")
-print(timeit.timeit(str(print(two_sum([1, 3, 7, 9, 2], 11)))))
+def two_sum3(nums, target):
+    """
+    Time: O(n) run
+    Space: O(n) space-complexity
+    :param nums: a list of integers
+    :param target: lookup value
+    :return: indexes of two item that sum-up to 'target
+    """
+    assist = []
+    for i in range(len(nums)):
+        assist.append(target - nums[i])
+        if target - nums[i] in nums:
+            idx = nums.index(target - nums[i])
+            if i != idx:
+                return [i, idx]
+
+    return None
+
+
+print("\n***** time-of-first-method ******")
+print(timeit.timeit(str(
+    print(two_sum([1, 11, 1, 11, 1, 2, 2, 2, 2, 44], 4)))))
 print("\n***** time-of-second-method ******")
-print(timeit.timeit(str(print(two_sum2([1, 3, 7, 9, 2], 11)))))
+print(timeit.timeit(str(
+    print(two_sum2([1, 11, 1, 11, 1, 2, 2, 2, 2, 44], 4)))))
+print("\n***** time-of-third-method ******")
+print(timeit.timeit(str(
+    print(two_sum3([1, 11, 1, 11, 1, 2, 2, 2, 2, 44], 4)))))
