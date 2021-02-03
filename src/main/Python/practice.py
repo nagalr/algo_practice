48b60a72e7a05508d1db3ae90f6ec0387029889e
Ronnen Nagal <nagalr@gmail.com>
2021-02-03 19:23:40 +0200

    Update the module

---
+++ src/main/Python/practice.py
@@ -0,0 +1,18 @@
+def minimumLoss(price):
+    i, j = price[0], price[len(price) - 1]
+    min_loss = 100000  # inf-figure
+    while i < j:
+        if 0 < price[i] - price[j] < min_loss:
+            min_loss = price[i] - price[j]
+
+        if price[i] > price[j]:
+            i += 1
+        else:
+            j -= 1
+
+    return min_loss
+
+
+l = [20, 7, 8, 2, 5]
+
+print(minimumLoss(l))
