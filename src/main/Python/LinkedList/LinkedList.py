d88060f849fc8315e5ddd2beee9bf644588313eb
Ronnen Nagal <nagalr@gmail.com>
2021-02-07 11:50:38 +0200

    blank commit

--- src/main/Python/LinkedList/LinkedList.py
+++ src/main/Python/LinkedList/LinkedList.py
@@ -68,3 +68,21 @@ class SinglyLinkedList:
 
 # ALL the methods are within the class,
 # to remove them, remove the 'self'
+
+# testing
+# testing
+Node1 = ListNode(1)
+Node2 = ListNode(2)
+Node3 = ListNode(3)
+Node4 = ListNode(4)
+Node5 = ListNode(5)
+
+head = Node1
+Node1.next = Node2
+Node2.next = Node3
+Node3.next = Node4
+Node4.next = Node5
+Node5.next = None
+
+
+
